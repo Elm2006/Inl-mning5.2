16e1c3f01676785441be0e1cc9e1622e20f5e141
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-22 10:14:41 +0100

    Add project files.

---
+++ Inlämning5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace inlämning5.__1
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många tal vill du skriva in");
+            int gånger = int.Parse(Console.ReadLine());
+            int[] tal = new int[gånger];
+            Console.WriteLine("Skriv in dina " + gånger + " heltal");
+            for (int i = 0; i < gånger; i++)
+            {
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i<gånger; i++)
+            {
+                Console.WriteLine("Tal"+(i+1)+": " + tal[i]);
+            }
+
+        }
+    }
+}
